016ffecbe263263110adda617f0cf3930a58965f
TheFabulousP <10421100@student.vgu.edu.vn>
2023-12-07 21:31:39 +0100

    create java files for classes

---
+++ Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+  public static void main(String[] args) {
+  }
+
+  public void say() {
+    System.out.println("I am an animal.");
+  }
+}
---
+++ BoardGame.java
@@ -0,0 +1,9 @@
+
+public class BoardGame {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    Animal animal = new Animal();
+    animal.say();
+  }
+}
